b54aaccc279dea1cd7475ea5f7a602d6af4b99e1
Abenthan <andresbenthan@gmail.com>
2022-05-05 07:33:22 -0500

    home /produccion /usuarios / operarios

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home/home');
 });
 
+// GET: produccion
+router.get('/produccion', (req, res) => {
+    res.render('home/produccion');
+});
 
+//GET: operarios
+router.get('/operarios', (req, res) => {
+    res.render('home/operarios');
+});
+
+//GET: Usuarios
+router.get('/usuarios', (req, res) => {
+    res.render('home/usuarios');
+});
 
 module.exports = router;
