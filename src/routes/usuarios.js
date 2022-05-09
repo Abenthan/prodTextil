219df9239894fc1c2efb3a00191c76c13f73b233
Abenthan <andresbenthan@gmail.com>
2022-05-09 05:36:08 -0500

    cambiar contraseña y eliminar usuario

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const pool = require('../database');
 const {isLoggedIn, isNotLoggedIn} = require('../lib/auth');
+const helpers = require('../lib/helpers');
 
 //GET: nuevoUsuario
 router.get('/nuevoUsuario', (req, res) => {
@@ -65,5 +66,50 @@ router.post('/editarUsuario/:id', async(req, res) => {
     req.flash('success', 'Usuario actualizado correctamente');
     res.redirect('/usuarios/listarUsuarios');   
 });
+
+//GET: cambiarPassword
+router.get('/cambiarPassword/:id', async(req, res) => {
+    const {id} = req.params;
+    const usuario = await pool.query('SELECT * FROM usuarios WHERE idUsuario = ?', [id]);
+    res.render('usuarios/cambiarPassword', {usuario: usuario[0]});
+});
+
+// POST: cambiarPassword
+router.post('/cambiarPassword/:id', async(req, res) => {
+    const {id} = req.params;
+    const {password, rePassword} = req.body;
+    if (password == rePassword){
+        console.log('ok, vamos a cambiar el password');
+        const newPassword = await helpers.encryptPassword(password);
+        const newUsuario = {
+            password: newPassword
+        };
+        await pool.query('UPDATE usuarios SET ? WHERE idUsuario = ?', [newUsuario, id]);
+    
+        req.flash('success', 'Contraseña actualizada correctamente');
+        res.redirect('/usuarios/listarUsuarios');
+    }else{
+        req.flash('message', 'Las contraseñas no coinciden');
+        res.redirect('/usuarios/cambiarPassword/' + id);
+    }
+
+    
+});
+
+//GET : eliminarUsuario
+router.get('/eliminarUsuario/:id', async(req, res) => {
+    const {id} = req.params;
+    const usuario = await pool.query('SELECT * FROM usuarios WHERE idUsuario = ?', [id]);
+    res.render('usuarios/eliminarUsuario', {usuario: usuario[0]});
+});
+
+// POST: eliminarUsuario
+router.post('/eliminarUsuario/:id', async(req, res) => {
+    const {id} = req.params;
+    await pool.query('DELETE FROM usuarios WHERE idUsuario = ?', [id]);
+    req.flash('success', 'Usuario eliminado correctamente');
+    res.redirect('/usuarios/listarUsuarios');
+});
+
 
 module.exports = router;
