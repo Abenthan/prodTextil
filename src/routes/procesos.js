eb558256106542c91f9ff42610e7d8f223ee34d0
Abenthan <andresbenthan@gmail.com>
2022-06-02 06:58:55 -0500

    preProduccion

--- src/routes/procesos.js
+++ src/routes/procesos.js
@@ -29,7 +29,7 @@ router.post('/editarProceso/:idProceso', async (req, res) => {
         estadoProceso: req.body.estado,
         observacionesProceso: req.body.observaciones
     };
-    
+
     // si proceso es mayor igual a 0, actualizamos
     if (proceso.cantidadOUT >= 0) {
         await pool.query('UPDATE procesos SET ? WHERE idProceso = ?', [proceso, idProceso]);
@@ -54,5 +54,48 @@ router.post('/editarProceso/:idProceso', async (req, res) => {
 
 });
 
+//GET: preProduccion
+router.get('/preProduccion/:idProceso', async (req, res) => {
+    const { idProceso } = req.params;
+    const resultadoProceso = await pool.query('SELECT * FROM procesos INNER JOIN ordenproduccion ON procesos.idOP = ordenproduccion.idOP WHERE idProceso = ?', [idProceso]);
+    const proceso = resultadoProceso[0];
+    res.render('procesos/preProduccion', { proceso });
+});
 
+// POST: preProduccion
+router.post('/preProduccion/:idProceso', async (req, res) => {
+
+    const { idProceso } = req.params;
+    const proceso = {
+        estadoProceso: req.body.estado,
+        observacionesProceso: req.body.observaciones
+    };
+
+    //actualizamos el proceso
+    await pool.query('UPDATE procesos SET ? WHERE idProceso = ?', [proceso, idProceso]);
+
+    const resultadoProceso = await pool.query('SELECT * FROM procesos WHERE idProceso = ?', [idProceso]);
+
+    // Actualizamos el estado en telares
+    if(proceso.estadoProceso == "Terminado"){ 
+        //actualizar estado de telares a "en cola"
+        await pool.query('UPDATE procesos SET estadoProceso = "en Cola" WHERE idOP = ? AND nombreProceso LIKE ?', [resultadoProceso[0].idOP, '%telar%']);
+
+        // actualizamos el estado de la OP
+        await pool.query('UPDATE ordenproduccion SET estadoOP = "en Proceso" WHERE idOP = ?', [resultadoProceso[0].idOP]);
+
+    }else{
+        //actualizar estado de telares a "pendiente" 
+        await pool.query('UPDATE procesos SET estadoProceso = "Pendiente" WHERE idOP = ? AND nombreProceso LIKE ?', [resultadoProceso[0].idOP, '%telar%']);
+
+        // actualizamos el estado de la OP
+        await pool.query('UPDATE ordenproduccion SET estadoOP = "Pendiente" WHERE idOP = ?', [resultadoProceso[0].idOP]);
+    }
+
+
+    req.flash('success', 'Proceso actualizado correctamente');
+
+    res.redirect('/procesos/preProduccion/' + idProceso);
+
+});
 module.exports = router;
